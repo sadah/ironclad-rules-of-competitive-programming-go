7777fa607ab2b40491548b8e382e9e5c208b0cec
sadah <sadaaki.hirai@gmail.com>
2024-01-06 00:59:37 +0000

    Add B02

--- src/B03/main.go
+++ src/B03/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -10,7 +11,17 @@ const YES = "Yes"
 const NO = "No"
 
 func solve(N int64, A []int64) {
-
+	for i := int64(0); i < N; i++ {
+		for j := i + 1; j < N; j++ {
+			for k := j + 1; k < N; k++ {
+				if A[i]+A[j]+A[k] == 1000 {
+					fmt.Println(YES)
+					return
+				}
+			}
+		}
+	}
+	fmt.Println(NO)
 }
 
 func main() {
@@ -20,12 +31,12 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var N int64
-    scanner.Scan()
-    N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    A := make([]int64, N)
-    for i := int64(0); i < N; i++ {
-        scanner.Scan()
-        A[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    }
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	A := make([]int64, N)
+	for i := int64(0); i < N; i++ {
+		scanner.Scan()
+		A[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	}
 	solve(N, A)
 }
